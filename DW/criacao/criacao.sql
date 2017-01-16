804cc40734574e5ad7e77ef5f551e7859fa5ada9
Ultimate <gil.fcp.2007@gmail.com>
2017-01-16 23:05:28 +0000

    data warehouse

---
+++ DW/criacao/criacao.sql
@@ -0,0 +1,140 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema DW
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema DW
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `DW` DEFAULT CHARACTER SET utf8 ;
+USE `DW` ;
+
+-- -----------------------------------------------------
+-- Table `DW`.`dim_Cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`dim_Cliente` (
+  `idCliente` INT NOT NULL AUTO_INCREMENT,
+  `nomeCliente` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `numeroTelemovel` VARCHAR(45) NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `pais` VARCHAR(45) NOT NULL,
+  `dataNascimento` DATE NOT NULL,
+  `dataRegisto` DATE NOT NULL,
+  `sexo` VARCHAR(1) NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`idCliente`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DW`.`dim_produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`dim_produto` (
+  `idproduto` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `precoBase` DECIMAL(10,5) NOT NULL,
+  `quantidadeDisponivel` INT NOT NULL,
+  `idadeParaJogar` INT NOT NULL,
+  `desconto` DECIMAL(10,5) NOT NULL,
+  `produtor` VARCHAR(45) NOT NULL,
+  `dataLancamento` DATE NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`idproduto`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DW`.`dim_Data`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`dim_Data` (
+  `idData` INT NOT NULL AUTO_INCREMENT,
+  `data` DATE NOT NULL,
+  `ano` INT NOT NULL,
+  `mes` INT NOT NULL,
+  `diaMes` INT NOT NULL,
+  `diaSemana` VARCHAR(45) NOT NULL,
+  `trimeste` INT NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`idData`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DW`.`ft_VendasJogos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`ft_VendasJogos` (
+  `idVendasJogos` INT NOT NULL AUTO_INCREMENT,
+  `quantidadeComprada` INT NOT NULL,
+  `precoCompradoUnidade` DECIMAL(10,5) NOT NULL,
+  `precoTotal` DECIMAL(10,5) NOT NULL,
+  `descontoVenda` DECIMAL(10,5) NOT NULL,
+  `idadeCliente` INT NOT NULL,
+  `idCliente` INT NOT NULL,
+  `idproduto` INT NOT NULL,
+  `idData` INT NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`idVendasJogos`),
+  INDEX `fk_ft_VendasJogos_dim_Cliente_idx` (`idCliente` ASC),
+  INDEX `fk_ft_VendasJogos_dim_produto1_idx` (`idproduto` ASC),
+  INDEX `fk_ft_VendasJogos_dim_Data1_idx` (`idData` ASC),
+  CONSTRAINT `fk_ft_VendasJogos_dim_Cliente`
+    FOREIGN KEY (`idCliente`)
+    REFERENCES `DW`.`dim_Cliente` (`idCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ft_VendasJogos_dim_produto1`
+    FOREIGN KEY (`idproduto`)
+    REFERENCES `DW`.`dim_produto` (`idproduto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ft_VendasJogos_dim_Data1`
+    FOREIGN KEY (`idData`)
+    REFERENCES `DW`.`dim_Data` (`idData`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DW`.`cliente_Historia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`cliente_Historia` (
+  `idCliente` INT NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `pais` VARCHAR(45) NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  INDEX `fk_cliente_Historia_dim_Cliente1_idx` (`idCliente` ASC),
+  CONSTRAINT `fk_cliente_Historia_dim_Cliente1`
+    FOREIGN KEY (`idCliente`)
+    REFERENCES `DW`.`dim_Cliente` (`idCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DW`.`produto_historia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DW`.`produto_historia` (
+  `idproduto` INT NOT NULL,
+  `precoBase` DECIMAL(10,5) NOT NULL,
+  `desconto` DECIMAL(10,5) NOT NULL,
+  `last_update` TIMESTAMP NOT NULL,
+  INDEX `fk_historia_Produto_dim_produto1_idx` (`idproduto` ASC),
+  CONSTRAINT `fk_historia_Produto_dim_produto1`
+    FOREIGN KEY (`idproduto`)
+    REFERENCES `DW`.`dim_produto` (`idproduto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
